3f73bd5d43d8a649dde459f2a81ac23395c6418e
WellingtonJr <wferreira.bm13@gmail.com>
2022-08-03 22:11:22 -0300

    configurando permissao de rotas

--- src/main/java/com/dio/springsecurity/WebSecurityConfig.java
+++ src/main/java/com/dio/springsecurity/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.dio.springsecurity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -21,6 +23,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("admin")
                 .password("{noop}master123")
                 .roles("MANAGERS");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers("/managers").hasAnyRole("MANAGERS")
+                .antMatchers("/users").hasAnyRole("USERS", "MANAGERS")
+                .anyRequest().authenticated().and().formLogin();
     }
 
 }
